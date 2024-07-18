7dc016298737893ce6ac337dd5fa8e364a25f20e
Berzan Tas <tberzan@gmail.com>
2024-07-18 11:28:32 +0200

    running database object

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from scheduleCreator import ScheduleCreator
+import database
 
 # main application
 class App:
     def __init__(self) -> None:
-        self.schedule_creator = ScheduleCreator()
+        self.db = database.EmployeeDB()
+        self.schedule_creator = ScheduleCreator(self.db)
 
     # main method
     def run(self):
+        self.db.main()
         # call main method of ScheduleCreater class
         self.schedule_creator.main()
 
