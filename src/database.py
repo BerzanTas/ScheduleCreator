941fb8fc7b8be4369c5be48dbbe29b2540c330f2
Berzan Tas <tberzan@gmail.com>
2024-09-06 11:37:10 +0200

    new Employee class, which allows creating, editing and removing employees from users database

--- src/database.py
+++ src/database.py
@@ -56,7 +56,54 @@ class Register:
             return False
         
 class EmployeeData:
-    pass
-        
+    def __init__(self):
+        self.api_url = sensitive_data.API_URL+'/data'
 
+    def checkIfTableExist(self, user_email):
+        data = {
+            "user_email": user_email
+        }
+        response = requests.post(self.api_url+'/checkemployee', json=data)
+        return response.status_code
+    
+    def getEmployeeTable(self, user_email):
+        data = {
+            "user_email": user_email
+        }
+        employee_data = requests.post(self.api_url+'/get', json=data).json()
+        try:
+            return employee_data["result"]
+        except KeyError:
+            return []
+
+    def addEmployee(self, user_email, employee_id, employee_name, working_time, student_or_second_job):
+        data = {
+            "user_email": user_email,
+            "employee_id": employee_id,
+            "employee_name": employee_name,
+            "work_time": working_time,
+            "student_or_second_job": student_or_second_job
+        }
+        response = requests.post(self.api_url+'/add', json=data)
+        print(response)
+    
+    def deleteEmployee(self, user_email, employee_id):
+        data = {
+            "user_email": user_email,
+            "employee_id": employee_id,
+        }
+        response = requests.post(self.api_url+'/delete', json=data)
+        print(response)
+
+    def updateEmployeeData(self, user_email, old_employee_id, employee_id, employee_name, working_time, student_or_second_job):
+        data = {
+            "user_email": user_email,
+            "old_employee_id": old_employee_id,
+            "employee_id": employee_id,
+            "employee_name": employee_name,
+            "work_time": working_time,
+            "student_or_second_job": student_or_second_job
+        }
+        response = requests.post(self.api_url+'/update', json=data)
+        print(response)
         
